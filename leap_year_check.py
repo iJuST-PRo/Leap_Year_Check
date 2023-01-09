899dc139a9c7552ff11504ceee1ce4f361aeea28
iJuST-PRo <lotfe119997@gmail.com>
2023-01-09 14:04:50 -0500

    Update script

--- leap_year_check.py
+++ leap_year_check.py
@@ -2,11 +2,11 @@
 class Leap():
     def isleap(year):
         if year % 4 == 0 and year % 100 != 0:
-            print(f'{year} is a leap year')
+            return print(f'{year} is a leap year')
         elif year % 400 == 0 and year % 100 == 0:
-            print(f'{year} is a leap year')
+            return print(f'{year} is a leap year')
         else:
-            print(f'{year} is not a leap year')
+            return print(f'{year} is not a leap year')
 
 
 if __name__=='__main__':
