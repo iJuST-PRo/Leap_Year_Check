099d717b6479ed8a118f742db912aa240a6e5615
iJuST-PRo <lotfe119997@gmail.com>
2023-01-09 12:22:44 -0500

    modify the class

--- leap_year_check.py
+++ leap_year_check.py
@@ -1,16 +1,19 @@
-def isleap(year):
-    if year % 4 == 0 and year % 100 != 0:
-        print(f'{year} is a leap year')
-    elif year % 400 == 0 and year % 100 == 0:
-        print(f'{year} is a leap year')
-    else:
-        print(f'{year} is not a leap year')
+
+class Leap():
+    def isleap(year):
+        if year % 4 == 0 and year % 100 != 0:
+            print(f'{year} is a leap year')
+        elif year % 400 == 0 and year % 100 == 0:
+            print(f'{year} is a leap year')
+        else:
+            print(f'{year} is not a leap year')
 
 
 if __name__=='__main__':
     try:
         year = int(input('Enter the year: '))
     except:
-        print('Enter a valid year')
+        print('Error: Please enter a valid year')
         exit()
-    isleap(year)
+
+    Leap.isleap(year)
